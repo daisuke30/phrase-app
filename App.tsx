6037ed0f3d65f519177b50d125e8809cd256407d
daisuke13 <d.katsuta1230@gmail.com>
2025-03-11 00:01:09 +0900

    add settings

---
+++ App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Provider as PaperProvider } from 'react-native-paper';
+import { Provider as StoreProvider } from 'react-redux';
+import { StatusBar } from 'expo-status-bar';
+import { store } from './src/store';
+import AppNavigator from './src/navigation/AppNavigator';
+import { theme } from './src/constants/theme';
+
+export default function App() {
+  return (
+    <StoreProvider store={store}>
+      <PaperProvider theme={theme}>
+        <SafeAreaProvider>
+          <AppNavigator />
+          <StatusBar style="auto" />
+        </SafeAreaProvider>
+      </PaperProvider>
+    </StoreProvider>
+  );
+}
